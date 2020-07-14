171c7613ff4056f41b32638b90fd40afeca03ef3
Jose Sustaita <josesustaita70@gmail.com>
2020-07-13 23:15:42 -0400

    fixed dot env and jwt signing

--- middleware/restrict.js
+++ middleware/restrict.js
@@ -1,21 +1,40 @@
-function restrict() {
-	return async (req, res, next) => {
-		const authError = {
-			message: "Invalid credentials",
-		}
+const jwt = require("jsonwebtoken");
 
-		try {
-			// express-session will automatically get the session ID from the cookie
-			// header, and check to make sure it's valid and the session for this user exists.
-			if (!req.session || !req.session.user) {
-				return res.status(401).json(authError)
-			}
+// use a scale for checking user roles since admin users
+// should still have access to normal user endpoints,
+// but normal users shouldn't have access to admin endpoints.
+const roles = ["normal", "admin"];
 
-			next()
-		} catch(err) {
-			next(err)
-		}
-	}
+function restrict(role) {
+  return async (req, res, next) => {
+    const authError = {
+      message: "Invalid credentials",
+    };
+
+    try {
+      // manually pull the token that got sent from the client's cookie jar
+      const token = req.cookies.token;
+      if (!token) {
+        return res.status(401).json(authError);
+      }
+
+      // checks to make sure the signature is valid and the token is not expired
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).json(authError);
+        }
+
+        // check if the role in our token is above or equal to the required role for the endpoint
+        if (role && roles.indexOf(decoded.userRole) < roles.indexOf(role)) {
+          return res.status(401).json(authError);
+        }
+
+        next();
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
 }
 
-module.exports = restrict
+module.exports = restrict;
